76e164a52d2e626886520db0a3de8d00983c4148
hadley <h.wickham@gmail.com>
2015-09-01 11:45:07 -0500

    Test UTF-8 output

--- tests/testthat/test-text.R
+++ tests/testthat/test-text.R
@@ -1,4 +1,5 @@
 context("text")
+library(xml2)
 
 test_that("special characters are escaped", {
   x <- xmlSVG({
@@ -8,4 +9,14 @@ test_that("special characters are escaped", {
   # xml_text unescapes for us - this still tests that the
   # file parses, which it wouldn't otherwise
   expect_equal(xml_text(xml_find_one(x, ".//text")), "<&>")
+})
+
+test_that("utf-8 characters are preserved", {
+  x <- xmlSVG({
+    plot.new()
+    text(0.5, 0.5, "\u00b5")
+  })
+  # xml_text unescapes for us - this still tests that the
+  # file parses, which it wouldn't otherwise
+  expect_equal(xml_text(xml_find_one(x, ".//text")), "\u00b5")
 })
