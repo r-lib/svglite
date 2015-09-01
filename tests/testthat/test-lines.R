f7e9679bf0e3e60384cc9984959a7772a46841dc
timelyportfolio <kent.russell@timelyportfolio.com>
2015-09-01 17:01:33 -0500

    add tests for stroke-dasharray with lty

--- tests/testthat/test-lines.R
+++ tests/testthat/test-lines.R
@@ -16,3 +16,36 @@ test_that("lines don't have fill", {
   })
   expect_equal(xml_attr(xml_find_one(x, ".//polyline"), "fill"), "none")
 })
+
+
+test_that("lines lty becomes stroke-dasharray", {
+  x <- xmlSVG({ plot(1:3,lty=1,type="l") })
+  expect_true(
+    is.na(xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray"))
+  )
+  x <- xmlSVG({ plot(1:3,lty=2,type="l") })
+  expect_equal(
+    xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray"),
+    "5,5"
+  )
+  x <- xmlSVG({ plot(1:3,lty=3,type="l") })
+  expect_equal(
+    xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray"),
+    "1,5"
+  )
+  x <- xmlSVG({ plot(1:3,lty=4,type="l") })
+  expect_equal(
+    xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray"),
+    "1,5,5,5"
+  )
+  x <- xmlSVG({ plot(1:3,lty=5,type="l") })
+  expect_equal(
+    xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray"),
+    "10,5"
+  )
+  x <- xmlSVG({ plot(1:3,lty=6,type="l") })
+  expect_equal(
+    xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray"),
+    "10,5,5,5"
+  )
+})
