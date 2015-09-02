1922cb7546563545896d6a659a7f083643a34935
timelyportfolio <kent.russell@timelyportfolio.com>
2015-09-02 08:30:27 -0500

    use mini_plot

--- tests/testthat/test-lines.R
+++ tests/testthat/test-lines.R
@@ -21,15 +21,15 @@ test_that("lines don't have fill", {
 
 test_that("lines lty becomes stroke-dasharray", {
   # base graphics
-  x <- xmlSVG(plot(1:3, lty = 1, type = "l"))
+  x <- xmlSVG(mini_plot(1:3, lty = 1, type = "l"))
   expect_equal(
-    xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray"),
+    xml_attr(xml_find_one(x,"//polyline"), "stroke-dasharray"),
     NA_character_
   )
 
   dash_array <- function(lty) {
-    x <- xmlSVG(plot(1:3, lty = lty, type = "l"))
-    xml_attr(suppressWarnings(xml_find_one(x, "//polyline")), "stroke-dasharray")
+    x <- xmlSVG(mini_plot(1:3, lty = lty, type = "l"))
+    xml_attr(xml_find_one(x, "//polyline"), "stroke-dasharray")
   }
   expect_equal(dash_array(2), "5,5")
   expect_equal(dash_array(3), "1,5")
