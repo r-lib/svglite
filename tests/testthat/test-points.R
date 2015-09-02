4af7fd397375e8b21d9c10b595b5393780abe88e
Håkon Malmedal <hmalmedal@gmail.com>
2015-09-02 22:21:45 +0200

    Specify fill/stroke opacity separately

--- tests/testthat/test-points.R
+++ tests/testthat/test-points.R
@@ -16,8 +16,10 @@ test_that("points get alpha stroke and fill given stroke and fill", {
     points(0.5, 0.5, pch = 21, col = rgb(1, 0, 0, 0.1), bg = rgb(0, 0, 1, 0.1), cex = 20)
   })
   circle <- xml_find_all(x, ".//circle")
-  expect_equal(xml_attr(circle, "stroke"), "rgba(255, 0, 0, 0.10)")
-  expect_equal(xml_attr(circle, "fill"), "rgba(0, 0, 255, 0.10)")
+  expect_equal(xml_attr(circle, "stroke"), rgb(1, 0, 0))
+  expect_equal(xml_attr(circle, "stroke-opacity"), "0.10")
+  expect_equal(xml_attr(circle, "fill"), rgb(0, 0, 1))
+  expect_equal(xml_attr(circle, "fill-opacity"), "0.10")
 })
 
 test_that("points are given stroke and fill", {
