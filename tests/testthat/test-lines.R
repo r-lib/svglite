4ecbc3577cd22646d455858a13b420cebbc1ade2
timelyportfolio <kent.russell@timelyportfolio.com>
2015-09-02 08:02:35 -0500

    remove lattice and ggplot2 b/c redundant

--- tests/testthat/test-lines.R
+++ tests/testthat/test-lines.R
@@ -21,10 +21,10 @@ test_that("lines don't have fill", {
 
 
 test_that("lines lty becomes stroke-dasharray", {
-  stroke_dash <- c( "5,5", "1,5", "1,5,5,5", "10,5", "10,5,5,5" )
+  stroke_dash <- c("5,5", "1,5", "1,5,5,5", "10,5", "10,5,5,5")
 
   # base graphics
-  x <- xmlSVG( plot( 1:3, lty = 1, type = "l" ) )
+  x <- xmlSVG(plot(1:3, lty = 1, type = "l"))
   expect_equal(
     xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray"),
     NA_character_
@@ -33,36 +33,9 @@ test_that("lines lty becomes stroke-dasharray", {
   expect_identical(
     sapply(
       2:6,
-      function( l ) {
-        x <- xmlSVG( plot( 1:3, lty = l, type = "l" ) )
+      function(l) {
+        x <- xmlSVG(plot( 1:3, lty = l, type = "l" ))
         xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray")
-      }
-    ),
-    stroke_dash
-  )
-
-  # lattice
-  expect_identical(
-    sapply(
-      2:6,
-      function( l ) {
-        x <- xmlSVG( { print( lattice::xyplot( x~x, data.frame( x = 1:3 ), lty = l, type = "l" ) ) } )
-        xml_attr(suppressWarnings(xml_find_all(x,"//polyline")[2]), "stroke-dasharray")
-      }
-    ),
-    stroke_dash
-  )
-
-  # ggplot2
-  expect_identical(
-    sapply(
-      2:6,
-      function( l ) {
-        x <- xmlSVG({
-          print( ggplot( data.frame( x = 1:3 ), aes( x = x,  y = x ) ) + geom_line( lty = l ) )
-        })
-        # this is brittle
-        xml_attr(suppressWarnings(xml_find_all(x,"//polyline"))[19], "stroke-dasharray" )
       }
     ),
     stroke_dash
