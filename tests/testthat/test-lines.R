dedc858b98976ef62e8d6003584b5d74bbeec0a9
timelyportfolio <kent.russell@timelyportfolio.com>
2015-09-02 08:09:48 -0500

    use @hadley much better,terser function

--- tests/testthat/test-lines.R
+++ tests/testthat/test-lines.R
@@ -1,7 +1,6 @@
 context("Lines")
 
 library(xml2)
-library(ggplot2)
 
 test_that("segments don't have fill", {
   x <- xmlSVG({
@@ -21,8 +20,6 @@ test_that("lines don't have fill", {
 
 
 test_that("lines lty becomes stroke-dasharray", {
-  stroke_dash <- c("5,5", "1,5", "1,5,5,5", "10,5", "10,5,5,5")
-
   # base graphics
   x <- xmlSVG(plot(1:3, lty = 1, type = "l"))
   expect_equal(
@@ -30,14 +27,13 @@ test_that("lines lty becomes stroke-dasharray", {
     NA_character_
   )
 
-  expect_identical(
-    sapply(
-      2:6,
-      function(l) {
-        x <- xmlSVG(plot( 1:3, lty = l, type = "l" ))
-        xml_attr(suppressWarnings(xml_find_one(x,"//polyline")), "stroke-dasharray")
-      }
-    ),
-    stroke_dash
-  )
+  dash_array <- function(lty) {
+    x <- xmlSVG(plot(1:3, lty = lty, type = "l"))
+    xml_attr(suppressWarnings(xml_find_one(x, "//polyline")), "stroke-dasharray")
+  }
+  expect_equal(dash_array(2), "5,5")
+  expect_equal(dash_array(3), "1,5")
+  expect_equal(dash_array(4), "1,5,5,5")
+  expect_equal(dash_array(5), "10,5")
+  expect_equal(dash_array(6), "10,5,5,5")
 })
