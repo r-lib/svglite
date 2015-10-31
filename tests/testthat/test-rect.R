b19359d479b4a691affd8ed40320597d0c0e8255
Yixuan Qiu <yixuan.qiu@cos.name>
2015-10-31 11:18:36 -0400

    missing function

--- tests/testthat/test-rect.R
+++ tests/testthat/test-rect.R
@@ -1,4 +1,12 @@
 context("Rect")
+library(xml2)
+
+style_attr <- function(nodes, attr) {
+  style <- xml_attr(nodes, "style")
+  ifelse(grepl(sprintf("%s: [^;]*;", attr), style),
+         gsub(sprintf(".*%s: ([^;]*);.*", attr), "\\1", style),
+         NA_character_)
+}
 
 test_that("rects equivalent regardless of direction", {
   x1 <- xmlSVG({
