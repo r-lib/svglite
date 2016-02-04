79499d985427f63fb6250915c6f6f72e2992f1c1
Yixuan Qiu <yixuanq@gmail.com>
2016-02-03 19:54:17 -0500

    code to test clipping

---
+++ tests/testthat/test-clip.R
@@ -0,0 +1,27 @@
+context("Clipping")
+
+test_that("create a plot that uses clipping", {
+  svglite("tests/testthat/test-clip.svg", 16, 8)
+  par(mfrow = c(1, 2))
+
+  set.seed(0)
+  mat <- matrix(runif(64), nrow = 8)
+
+  plot(0, 0, xlim = c(-1, 1), ylim = c(-1, 1), asp = 1, main = "Without clipping")
+  rect(-0.5, -0.5, 0.5, 0.5, col = "blue")
+  text(0, 0.5, "Clipping", cex = 5, srt = 30)
+  abline(h = 0.5, col = "red")
+  rasterImage(mat, -0.5, -1, 0.5, -0.5, angle = -30)
+
+  plot(0, 0, xlim = c(-1, 1), ylim = c(-1, 1), asp = 1, main = "With clipping")
+  clip(-1, 0, -1, 0)
+  rect(-0.5, -0.5, 0.5, 0.5, col = "blue")
+  clip(0, 1, 0, 1)
+  text(0, 0.5, "Clipping", cex = 5, srt = 30)
+  clip(-1, 0, 0, 1)
+  abline(h = 0.5, col = "red")
+  clip(0, 1, -1, 0)
+  rasterImage(mat, -0.5, -1, 0.5, -0.5, angle = -30)
+
+  dev.off()
+})
