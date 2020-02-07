44e4b15f49a5b137b904fc367db6977e3f02395a
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-02-07 21:02:41 +0100

    Fix creation of file during example run

--- R/SVG.R
+++ R/SVG.R
@@ -39,7 +39,7 @@
 #' @seealso \code{\link{pictex}}, \code{\link{postscript}}, \code{\link{Devices}}
 #' @examples
 #' # Save to file
-#' svglite("Rplots.svg")
+#' svglite(tempfile("Rplots.svg"))
 #' plot(1:11, (-5:5)^2, type = 'b', main = "Simple Example")
 #' dev.off()
 #'
@@ -48,7 +48,7 @@
 #'
 #' # Then supply a list of aliases:
 #' fonts <- list(sans = "Verdana", mono = "Times New Roman")
-#' svglite("Rplots.svg", system_fonts = fonts)
+#' svglite(tempfile("Rplots.svg"), system_fonts = fonts)
 #' plot.new()
 #' text(0.5, 0.5, "Some text", family = "mono")
 #' dev.off()
