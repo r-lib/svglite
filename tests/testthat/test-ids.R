b1c0147f751b08bfacf3623ad8288d8a604a22df
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-06-11 10:26:19 +0200

    update test

--- tests/testthat/test-ids.R
+++ tests/testthat/test-ids.R
@@ -14,7 +14,6 @@ test_that("ids are assigned as expecter", {
   expect_identical(svg[1], svg[2])
 
   expect_true(grepl("id='test'", svg[1]))
-  expect_true(grepl("#test line, #test polyline, #test polygon, #test path, #test rect, #test circle", svg[1]))
 
   sd <- svgstring(id = c("test", "test2"))
   plot(1:10, 1:10)
@@ -24,11 +23,8 @@ test_that("ids are assigned as expecter", {
   svg <- sd()
 
   expect_true(grepl("id='test'", svg[1]))
-  expect_true(grepl("#test line, #test polyline, #test polygon, #test path, #test rect, #test circle", svg[1]))
 
   expect_true(grepl("id='test2'", svg[2]))
-  expect_true(grepl("#test2 line, #test2 polyline, #test2 polygon, #test2 path, #test2 rect, #test2 circle", svg[2]))
 
   expect_false(grepl("id='test", svg[3]))
-  expect_true(grepl("line, polyline, polygon, path, rect, circle", svg[3]))
 })
