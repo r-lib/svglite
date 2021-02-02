bad99720a16f939c062497ff123c52d92badd351
Thomas Lin Pedersen <thomasp85@gmail.com>
2021-02-02 14:33:01 +0100

    reexport font registration from systemfonts

---
+++ R/aaa.R
@@ -0,0 +1,11 @@
+#' @importFrom systemfonts register_variant
+#' @export
+systemfonts::register_variant
+
+#' @importFrom systemfonts register_font
+#' @export
+systemfonts::register_font
+
+#' @importFrom systemfonts font_feature
+#' @export
+systemfonts::font_feature
