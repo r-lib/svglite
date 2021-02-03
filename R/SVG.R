e8f342f7acbec4d8f1adada3f739771cbf02a32e
Thomas Lin Pedersen <thomasp85@gmail.com>
2021-02-03 13:15:25 +0100

    Remove old font functionality from examples

--- R/SVG.R
+++ R/SVG.R
@@ -65,23 +65,12 @@
 #'   \email{jakeluciani@@yahoo.com} 2012: updated by Matthieu Decorde
 #'   \email{matthieu.decorde@@ens-lyon.fr}
 #' @seealso \code{\link{pictex}}, \code{\link{postscript}}, \code{\link{Devices}}
+#'
 #' @examples
 #' # Save to file
 #' svglite(tempfile("Rplots.svg"))
 #' plot(1:11, (-5:5)^2, type = 'b', main = "Simple Example")
 #' dev.off()
-#'
-#' # Supply system font aliases. First check the font can be located:
-#' systemfonts::match_font("Verdana")
-#'
-#' # Then supply a list of aliases:
-#' fonts <- list(sans = "Verdana", mono = "Times New Roman")
-#' svglite(tempfile("Rplots.svg"), system_fonts = fonts)
-#' plot.new()
-#' text(0.5, 0.5, "Some text", family = "mono")
-#' dev.off()
-#'
-#' # See the fonts vignettes for more options to deal with fonts
 #'
 #' @keywords device
 #' @useDynLib svglite, .registration = TRUE
