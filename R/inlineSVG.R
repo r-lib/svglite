ccae5f3545ac02a5fe078e4e40d3ea6f4f9d808a
Thomas Lin Pedersen <thomasp85@gmail.com>
2021-02-18 14:59:21 +0100

    fix examples using htmltools::browsable

--- R/inlineSVG.R
+++ R/inlineSVG.R
@@ -1,4 +1,4 @@
-#' Run plotting code and view svg in RStudio Viewer or web broswer.
+#' Run plotting code and view svg in RStudio Viewer or web browser.
 #'
 #' This is useful primarily for testing. Requires the \code{htmltools}
 #' package.
@@ -8,7 +8,7 @@
 #' @keywords internal
 #' @export
 #' @examples
-#' if (require("htmltools")) {
+#' if (interactive() && require("htmltools")) {
 #'   htmlSVG(plot(1:10))
 #'   htmlSVG(hist(rnorm(100)))
 #' }
