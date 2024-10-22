c5fafdbdf65dfd86a9ae778275713cfe3b8a55c4
Thomas Lin Pedersen <thomasp85@gmail.com>
2024-10-22 15:58:23 +0200

    update tests

--- tests/testthat/test-rect.R
+++ tests/testthat/test-rect.R
@@ -21,7 +21,7 @@ test_that("fill and stroke colors", {
     plot.new()
     rect(0.2, 0.2, 0.8, 0.8, col = "blue", border = "red")
   })
-  rectangle <- xml_find_all(x, "./g/rect")[[1]]
+  rectangle <- xml_find_all(x, "./g/g/rect")[[1]]
   expect_equal(style_attr(rectangle, "fill"), rgb(0, 0, 1))
   expect_equal(style_attr(rectangle, "stroke"), rgb(1, 0, 0))
 })
